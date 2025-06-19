9582161b7e0117b34f59218c32e5e0e7a437895e
jprjpr679 <jpr679@gmail.com>
2025-06-19 16:59:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
 
 const name = core.getInput('name');
-const output_value = 'Hello ${name} from Barsoom!';
+const output_value = 'Hello ${name}';
 core.setOutput('greeting', output_value);
